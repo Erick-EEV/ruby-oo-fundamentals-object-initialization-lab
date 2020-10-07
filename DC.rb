d6bb5addf0eaf1d2520ff93762a1d81a5a15a1ce
Erick-EEV <EEVERICKEEV@gmail.com>
2020-10-07 10:05:38 -0400

    Done.

---
+++ DC.rb
@@ -0,0 +1,53 @@
+require 'pry'
+class Animal
+    def species
+      @species
+    end
+    def species=(value)
+        @species = value
+    end
+end 
+
+  cow = Animal.new
+cow.species = "cow"
+puts cow.species
+
+dog = Animal.new
+dog.species = "dog"
+puts dog.species
+
+class Animal
+
+    def species
+      "cat"
+    end
+  end
+  
+  maru = Animal.new
+  hanna = Animal.new 
+
+class Animal
+    def species 
+      @my_species = "cat"
+    end
+  
+    def say_species
+      p "Hi! I'm a #{@my_species}"
+    end
+end 
+
+maru = Animal.new
+maru.species
+maru.say_species 
+
+class Animal
+    def initialize(value)
+        @species = value
+    end
+    def species
+        @species
+    end
+end
+
+frederick = Animal.new("bull")
+puts frederick.species
--- lib/dog.rb
+++ lib/dog.rb
@@ -3,13 +3,15 @@ class Dog
     def initialize(name_of_dog)
      @name = name_of_dog
     end
-        def initialize(breed)
- @breed = breed
-        if breed == @breed
-            puts @breed
-        else
-            binding.pry
-            puts "Mutt"
-            end
-        end
+#         def initialize(breed)
+#  @breed = breed
+#         if breed == @breed
+#             puts @breed
+#         else
+#             binding.pry
+#             puts "Mutt"
+#             end
+#         end
 end
+
+
